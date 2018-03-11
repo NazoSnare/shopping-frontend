7ea1d605ecd84bcd8964a278e75ae069e97e2d6e
malcom <nazoment@gmail.com>
2018-03-11 12:45:40 +0200

    finished registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FlashMessagesModule } from 'angular2-flash-messages';
 
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,9 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProductsComponent } from './products/products.component';
+
+import {ValidateService} from './validate.service';
+import { AuthService } from './auth.service';
 
 const appRoutes: Routes = [
   {path:'', component:HomeComponent},
@@ -36,9 +40,10 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
   ],
-  providers: [],
+  providers: [ValidateService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../validate.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,10 +15,54 @@ export class RegisterComponent implements OnInit {
    email: String;
    password: String;
    balance: Number;
-   
-  constructor() { }
+   messageTimeout = 3000;//timeout for flash messages in seconds
+
+  constructor(private validateService:ValidateService,
+                     private flashMessagesService: FlashMessagesService,
+                     private authService: AuthService,
+                     private router:Router
+                   ) { }
 
   ngOnInit() {
   }
+
+  onRegisterSubmit(){
+    const user = {
+      name: this.name,
+      email: this.email,
+      username: this.username,
+      password: this.password,
+      balance: this.balance
+    }
+      //required fields
+      if(!this.validateService.validateRegister(user)){
+        this.flashMessagesService.show('Please fill in all fields!',
+         { cssClass: 'alert-danger', timeout: this.messageTimeout });
+        return false;
+      }
+
+      //validate validateEmail
+      if(!this.validateService.validateEmail(user.email)){
+        this.flashMessagesService.show('Please enter valid email!',
+         { cssClass: 'alert-danger', timeout: this.messageTimeout });
+        return false;
+      }
+      //register users
+       this.authService.registerUser(user).subscribe(data => {
+         if(data.success){
+           this.flashMessagesService.show(data.msg,
+            { cssClass: 'alert-success', timeout: this.messageTimeout });
+            this.router.navigate(['/login']);
+         }
+         else{
+           this.flashMessagesService.show(data.msg,
+            { cssClass: 'alert-success', timeout: this.messageTimeout });
+         }
+       });
+
+        //
+
+
+  }//end of register submit
 
 }
